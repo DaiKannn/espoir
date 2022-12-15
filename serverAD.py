9c4812ee4dfb5bf0d16e8337e0248171da2d6f48
DaiKannn <94958146+DaiKannn@users.noreply.github.com>
2022-12-15 01:41:07 +0100

    Update serverAD.py

--- serverAD.py
+++ serverAD.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 import subprocess
 import platform
 import psutil
@@ -25,66 +26,75 @@ def serveur():
                     while msg != "kill" and msg != "reset" and msg != "disconnect":
                         msg = conn.recv(1024).decode()
                         print ("Received from client: ", msg)
-                        if msg == "DOS:dir":
-                            message = subprocess.getoutput('dir')
-                            conn.send(message.encode())
-                            print("Voici le dir")
+                        if msg == 'OS':
+                            conn.send(f"{platform.system()}".encode())
 
-                        elif msg == "DOS:mkdir toto":
-                            message = subprocess.getoutput('mkdir toto')
-                            conn.send(message.encode())
-                            print("Voici le mkdir")
+                        elif msg == 'CPU':
+                            nbr_cpu = psutil.cpu_count()
+                            cpus = psutil.cpu_percent()
+                            cpusfinal = str(cpus)
+                            conn.send(
+                                f"Nombre de CPU logiques dans le système : {nbr_cpu}, Utilisation de tous les CPU : {cpusfinal}%".encode())
 
-                        elif msg == "Linux:ls -la":
-                            message = subprocess.getoutput('ls -la')
-                            conn.send(message.encode())
-                            print("Voici le ls -la")
 
-                        elif msg == "Powershell:get-process":
-                            message = subprocess.getoutput('get-process')
-                            conn.send(message.encode())
-                            print("Voici le get-process")
+                        elif msg == 'RAM':
+                            meminfo = psutil.virtual_memory()
+                            total = round(meminfo.total / 1_073_741_824, 2)
+                            utilise = round(meminfo.used / 1_073_741_824, 2)
+                            disponible = round(meminfo.free / 1_073_741_824, 2)
+                            conn.send(
+                                f"Total de RAM : {total} Go, RAM utilsé : {utilise} Go, RAM disponible : {disponible} Go".encode())
+
+
+                        elif msg == 'IP':
+                            ipa = []
+                            for nic, addrs in psutil.net_if_addrs().items():
+                                for addr in addrs:
+                                    address = addr.address
+                                    if addr.family == AF_INET and not address.startswith("169.254"):
+                                        ipa.append(f"{address}/{IPv4Network('0.0.0.0/' + addr.netmask).prefixlen}")
+                            ipfinal = str(ipa)[1:-1]
+                            conn.send(f"IP de la machine : {ipfinal}".encode())
+
+
+                        elif msg == 'Name':
+                            conn.send(f"Nom de la machine : {platform.node()}".encode())
+
+
+
+                        elif msg == 'Connexion information':
+                            conn.send(
+                                f"Le nom de la machine est {platform.node()}, son IP est la suivante : {socket.gethostbyname(socket.gethostname())}".encode())
+
+
+
+                        elif msg[0:4] == "Linux:":
+                            if sys.platform == 'linux':
+                                divise = msg.split(':')[1]
+                                resultat = subprocess.check_output(divise, shell=True).decode('cp850')
+                                conn.send(f"Commande {divise} : {resultat}".encode())
+                            else:
+                                conn.send("Commande échouée : OS inadéquat".encode())
+
+
+                        elif msg[0:4] == "DOS:":
+                            if sys.platform == 'win32':
+                                divise = msg.split(':')[1]
+                                resultat = subprocess.check_output(divise, shell=True).decode('cp850')
+                                conn.send(f"Commande {divise} : {resultat}".encode())
+                            else:
+                                conn.send("Commande échouée : OS inadéquat".encode())
+
 
                         elif msg == "python --version":
-                            message = subprocess.getoutput('version')
-                            conn.send(message.encode())
-                            print("Voici la version de python")
+                            python = subprocess.getoutput('python --version')
+                            conn.send(f"La version de python est la suivante : {python}".encode())
 
-                        elif msg == "ping 192.157.65.78":
-                            message = subprocess.getoutput('ping')
-                            conn.send(message.encode())
-                            print("Voici le ping")
 
-                        elif msg == "RAM Use":
-                            message = subprocess.getoutput(psutil.virtual_memory().percent)
-                            conn.send(message.encode())
-                            print("Voici la RAM utilisé")
-
-                        elif msg == "RAM restante":
-                            message = subprocess.getoutput(
-                            psutil.virtual_memory().available * 100 / psutil.virtual_memory().total)
-                            conn.send(message.encode())
-                            print("Voici la RAM restante")
-
-                        elif msg == "CPU Use":
-                            message = subprocess.getoutput(psutil.cpu_percent())
-                            conn.send(message.encode())
-                            print("Voici le % CPU utilisé")
-
-                        elif msg == "OS":
-                            message = subprocess.getoutput(platform.system())
-                            conn.send(message.encode())
-                            print("Voici l'OS")
-
-                        elif msg == "Nom de Machine":
-                            message = subprocess.getoutput(socket.gethostname())
-                            conn.send(message.encode())
-                            print("Voici le nom de la machine")
-
-                        elif msg == "Adresse IP":
-                            message = subprocess.getoutput(socket.gethostbyname(socket.gethostname()))
-                            conn.send(message.encode())
-                            print("Voici l'adresse IP")
+                        elif msg[0:4] == 'ping':
+                            p = msg.split(' ')
+                            destinataire = p[1]
+                            conn.send(subprocess.getoutput('ping ' + destinataire).encode())
                 except ConnectionResetError:
                     print("")
                 except KeyboardInterrupt:
